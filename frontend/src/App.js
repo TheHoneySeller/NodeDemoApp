f00b29ae027b2d116537f112050b4cb95690457a
Faruk Balci <farukbalci99@gmail.com>
2024-01-23 15:34:55 +0300

    Finish project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/esm/Col'
 import { useEffect, useState } from 'react';
 import axios from 'axios'
+import { BarChart } from '@mui/x-charts/BarChart'
 
 function VendorForm({setVendorId}) {
   function handleSubmit(event) {
@@ -62,9 +63,42 @@ function ProductTable({salesData}) {
   )
 }
 
+function MonthlySalesChart({monthlySalesData}) {
+  const xAxisLabels = monthlySalesData.map( (element) => toDateString(element._id.year, element._id.month) )
+  const numberOfSales = monthlySalesData.map( (element) => element.total_sales_month ) 
+
+  console.log(xAxisLabels)
+  console.log(numberOfSales)
+
+  return (
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: xAxisLabels}]}
+      series={[{data: numberOfSales}]}
+    />
+  )
+}
+
+function toDateString(year, month) {
+  const date = new Date(year, month - 1)
+  const monthName = date.toLocaleString('default', { month: 'long' })
+  return `${monthName} ${year}`
+}
+
 function App() {
+
+  const dummyMonthlySalesData = 
+  [{
+      "_id": {
+          "year": 0,
+          "month": 1
+      },
+      "total_sales_month": 0
+  }]
+  
+
   const [vendorId, setVendorId] = useState()
-  const [productSales, setProductSales] = useState([]) 
+  const [productSales, setProductSales] = useState([])
+  const [monthlySales, setMonthlySales] = useState(dummyMonthlySalesData)
   
   useEffect( () => {
     //to do: get connection constants from a config file.
@@ -75,14 +109,32 @@ function App() {
       } ).catch((error)=>{
         console.log(error)
       })
+      axios.get("http://localhost:8080/monthlysales?vendorId=" + vendorId).then( (response) => {
+        console.log(response.data)
+        if (response.data.length === 0) {
+          setMonthlySales(dummyMonthlySalesData)
+        } else {
+          setMonthlySales(response.data)
+        }
+        
+      } ).catch((error)=>{
+        console.log(error)
+      })
     }
     
   }, [vendorId] )
 
   return (
     <div className="justify-content-center mx-5">
-        <VendorForm setVendorId={setVendorId }/>
-        <ProductTable salesData={productSales}/>
+        <div className='vendor-form'>
+         <VendorForm setVendorId={setVendorId }/>
+        </div>
+        <div className='product-table' >
+          <ProductTable salesData={productSales}/>
+        </div>
+        <div className='sales-chart'>
+          <MonthlySalesChart monthlySalesData={monthlySales}/>
+        </div>
     </div>
   );
 }
